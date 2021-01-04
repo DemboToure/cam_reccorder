24b89508fb8f64d84be03ef62c0737b118e6e0d5
DemboToure <dembotoure00@gmail.com>
2021-01-04 13:11:27 +0000

    test commit

--- processing/process.py
+++ processing/process.py
@@ -3,6 +3,6 @@ from video_utils import video_utils
 
 if __name__ == '__main__' :
     logger.debug("Running...")
-    #handle_image('source_data/walter.jpg')
+    video_utils.handle_image('source_data/walter.jpg')
     #cv2.destroyAllWindows()
-    video_utils.read_cam_save(0)
+    #video_utils.read_cam_save(0)
