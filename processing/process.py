2f8bb1ef9cb937c84ae0b37a8f82910c74621d12
DemboToure <dembotoure00@gmail.com>
2021-01-04 11:43:59 +0000

    first commit

---
+++ processing/process.py
@@ -0,0 +1,8 @@
+from logger.log import logger
+from video_utils import video_utils 
+
+if __name__ == '__main__' :
+    logger.debug("Running...")
+    #handle_image('source_data/walter.jpg')
+    #cv2.destroyAllWindows()
+    video_utils.read_cam_save(0)
